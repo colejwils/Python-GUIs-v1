d165c2d5ba505fff1d7f3490cdf2b0321b3e7223
Cole Wilson <cole@colejwils.com>
2024-05-26 09:13:53 -0400

    Update playing_around.py

--- playing_around.py
+++ playing_around.py
@@ -13,9 +13,9 @@ import random
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 class PopUpWindow():
-    def __init__(self, main_window):
+    def __init__(self, main_window, title="Pop Up Window", labels=[], entries=[], buttons=[]):
         self.pop_up_window = tk.Toplevel()
-        self.pop_up_window.title("Pop Up Window")
+        self.pop_up_window.title(title)
         self.width = 400
         self.height = 200
         self.screen_width = self.pop_up_window.winfo_screenwidth()
@@ -38,7 +38,11 @@ class MainWindow():
         self.y = (self.screen_height / 2) - (self.height / 2)
         self.root.geometry("%dx%d+%d+%d" % (self.width, self.height, self.x, self.y))
         self.root.resizable(True, True)
-
+        self.canvas = tk.Canvas(self.root, width=1150, height=600)
+        self.canvas.grid(row=0, column=0, sticky="nsew")
+        self.inner_frame = ttk.Frame(self.canvas)
+        self.inner_frame.grid(row=0, column=0, sticky="nsew")
+        self.canvas.create_window(0, 0, window=self.inner_frame, anchor="nw")
 
     def start_program(self):
         self.root.mainloop()
@@ -46,4 +50,5 @@ class MainWindow():
 
 main_window = MainWindow()
 pop_up_window = PopUpWindow(main_window)
+test_label = ttk.Label(pop_up_window.pop_up_window, text="Test Label")
 main_window.start_program()
